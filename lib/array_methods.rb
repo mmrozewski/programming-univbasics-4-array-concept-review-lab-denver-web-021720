f331482d723d824a495b4ad1da1f6d467775363f
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-07 01:26:01 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,8 @@
 def find_element_index(array, value_to_find)
-  n = 0
-  if array.include?(value_to_find) then 
-    while array[n] != value_to_find do
-      n += 1 
-    end
-    return n
+  if array.include?(value_to_find) 
+    return array.index(value_to_find)
   else 
-    return nil
+    nil
   end 
     
 end
@@ -18,3 +14,5 @@ end
 def find_min_value(array)
   return array.sort.first
 end
+
+#start
